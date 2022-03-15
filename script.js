6e27e99bc3a2197b4403e0a615d574a0b53af730
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-03-15 15:03:11 +0100

    chatlist con vue

---
+++ script.js
@@ -0,0 +1,42 @@
+const app = new Vue (
+    {
+        el:"#app",
+        data: {
+            chat: [
+                {
+                    name: "Marco",
+                    foto: "img/avatar_1.jpg"
+                },
+                {
+                    name: "Fabio",
+                    foto: "img/avatar_2.jpg"
+                },
+                {
+                    name: "Samuele",
+                    foto: "img/avatar_3.jpg"
+                },
+                {
+                    name: "Alessandro B.",
+                    foto: "img/avatar_4.jpg"
+                },
+                {
+                    name: "Alessandro L.",
+                    foto: "img/avatar_5.jpg"
+                },
+                {
+                    name: "Claudia",
+                    foto: "img/avatar_6.jpg"
+                },
+                {
+                    name: "Federico",
+                    foto: "img/avatar_7.jpg"
+                },
+                {
+                    name: "Davide",
+                    foto: "img/avatar_8.jpg"
+                }
+            ]
+        }
+    }
+) 
+    
